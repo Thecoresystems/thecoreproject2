9b90b9506d50797baf45a006ea497d5a959791cd
Thecoresystems <anchal.thecore@gmail.com>
2025-02-07 12:54:41 +0530

    Initial commit

---
+++ 2sadacode.py
@@ -0,0 +1,18 @@
+import time
+print("hello world")
+time.sleep(4)
+print("-------------")
+time.sleep(4)
+print("hello balle balle")
+print("hello balle balle")
+print("hello balle balle")
+print("hello balle balle")
+time.sleep(4)
+i=0
+while 1:
+    print("     --sADA I am inside loop")
+    time.sleep(0.5)
+    i=i+1
+    print(i)
+        
+        
---
+++ 3code.py
@@ -0,0 +1,15 @@
+import time
+print("hello world")
+time.sleep(4)
+print("-------------")
+time.sleep(4)
+print("hello kiran")
+time.sleep(4)
+i=0
+while 1:
+    print("     --sADA I am inside loop")
+    time.sleep(0.5)
+    i=i+1
+    print(i)
+        
+        
