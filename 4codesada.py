b08abae318de9c2e075528436078feb1e5ee3d4a
Thecoresystems <anchal.thecore@gmail.com>
2025-02-07 12:57:05 +0530

    Initial commit

---
+++ 4codesada.py
@@ -0,0 +1,14 @@
+import time
+
+def myprintfunction():
+    print("Hello World")
+    time.sleep(2)
+    print("----------------")
+    print("this is my created function  1 ")
+    print(" kiddan jaanab ki banda _________")
+    
+    
+while 1:
+    print (" we are inside loop ")
+    print("_________________________")
+    myprintfunction()
